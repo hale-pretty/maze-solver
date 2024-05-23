0f1596415e27c98e3277163388ce326925f8468e
hale-pretty <lnhatha89@gmail.com>
2024-05-23 16:16:09 +0700

    final version

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from maze import Maze
 
 win = Window(1200, 1200)
 
-m1 = Maze(10, 10, 10, 8, 50, 50, win)
+m1 = Maze(100, 100, 8, 10, 100, 100, win)
 m1.create_cells()
 m1.break_entrance_and_exit()
 m1.break_walls_r(0, 0)
-# m1.reset_cells_visited()
-
+m1.reset_cells_visited()
+m1.solve()
 
 win.wait_for_close()
--- src/maze.py
+++ src/maze.py
@@ -36,61 +36,94 @@ class Maze:
 
     def create_cells(self) -> List[List[Cell]]:
         self.cells = []
-        pointer_x = self.x1
-        for i in range(self.num_cols):
-            pointer_y = self.y1
-            new_col = []
-            self.cells.append(new_col)
-            for j in range(self.num_rows):
-                new_col.append(Cell(pointer_x, pointer_x + self.cell_size_x,
+        pointer_y = self.y1
+        for i in range(self.num_rows):
+            pointer_x = self.x1
+            new_row = []
+            self.cells.append(new_row)
+            for j in range(self.num_cols):
+                new_row.append(Cell(pointer_x, pointer_x + self.cell_size_x,
                                          pointer_y, pointer_y + self.cell_size_y,
                                          self.win))
                 self.draw_cell(i, j)
-                pointer_y += self.cell_size_y
-            pointer_x += self.cell_size_x
+                pointer_x += self.cell_size_x
+            pointer_y += self.cell_size_y
         return self.cells 
 
     def break_entrance_and_exit(self):
         self.cells[0][0].has_left_wall = False
-        self.cells[self.num_cols - 1][self.num_rows - 1].has_right_wall = False
+        self.cells[self.num_rows - 1][self.num_cols - 1].has_right_wall = False
         self.draw_cell(0, 0)
-        self.draw_cell(self.num_cols - 1, self.num_rows - 1)      
+        self.draw_cell(self.num_rows - 1, self.num_cols - 1)      
 
     def reset_cells_visited(self):
-        for i in range(self.num_cols):
-            for j in range(self.num_rows):
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
                 self.cells[i][j].visited = False
 
     def break_walls_r(self, i: int, j: int):
         self.cells[i][j].visited = True
-        dir = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         neighbors = []
-        for (dir_x, dir_y) in dir:
+        for idx, (dir_x, dir_y) in enumerate(directions):
             adj_x = i + dir_x
             adj_y = j + dir_y
-            if (adj_x < 0 or adj_x >= self.num_cols 
-                or adj_y < 0 or adj_y >= self.num_rows 
-                or self.cells[adj_x][adj_y].visited is True):
-                neighbors.append(None)
-            else:
-                
-        choosen_dir = neighbors[random.randrange(0, len(neighbors))]
-        if choosen_dir == dir[0]:
-            self.cells[i][j].has_right_wall = False
-            self.cells[choosen_dir[0]][choosen_dir[1]].has_left_wall = False
-            
-        elif choosen_dir == dir[1]:
-            self.cells[i][j].has_left_wall = False
-            self.cells[choosen_dir[0]][choosen_dir[1]].has_right_wall = False
-        elif choosen_dir == dir[2]:
-            self.cells[i][j].has_bottom_wall = False
-            self.cells[choosen_dir[0]][choosen_dir[1]].has_top_wall = False
-        elif choosen_dir == dir[3]:
-            self.cells[i][j].has_top_wall = False
-            self.cells[choosen_dir[0]][choosen_dir[1]].has_bottom_wall = False
+            if (adj_x < 0 or adj_x >= self.num_rows 
+                or adj_y < 0 or adj_y >= self.num_cols):
+                continue
+            neighbors.append((adj_x, adj_y, idx))
+        while len(neighbors) != 0:
+            choosen_dir = neighbors.pop(random.randrange(len(neighbors)))
+            if self.cells[choosen_dir[0]][choosen_dir[1]].visited:
+                continue
+            if choosen_dir[2] == 0:
+                self.cells[i][j].has_right_wall = False
+                self.cells[choosen_dir[0]][choosen_dir[1]].has_left_wall = False
+            elif choosen_dir[2] == 1:
+                self.cells[i][j].has_left_wall = False
+                self.cells[choosen_dir[0]][choosen_dir[1]].has_right_wall = False
+            elif choosen_dir[2] == 2:
+                self.cells[i][j].has_bottom_wall = False
+                self.cells[choosen_dir[0]][choosen_dir[1]].has_top_wall = False
+            elif choosen_dir[2] == 3:
+                self.cells[i][j].has_top_wall = False
+                self.cells[choosen_dir[0]][choosen_dir[1]].has_bottom_wall = False
+            self.break_walls_r(choosen_dir[0], choosen_dir[1])
         self.draw_cell(i, j)
-        self.draw_cell(choosen_dir[0], choosen_dir[1])
-        self.break_walls_r(choosen_dir[0], choosen_dir[1])
+
+    def solve_r(self, row: int, col: int):
+        self.animate()
+        self.cells[row][col].visited = True
+        if row == self.num_rows - 1 and col == self.num_cols - 1:
+            out_x = self.x1 + self.cell_size_x * self.num_cols
+            out_y = self.y1 + self.cell_size_y * (self.num_rows - 1)
+            self.cells[row][col].draw_move(Cell(out_x, out_x + self.cell_size_x, out_y, out_y + self.cell_size_y, self.win))
+            return True
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        for i in range(4):
+            (dir_x, dir_y) = directions[i]
+            adj_x = row + dir_x
+            adj_y = col + dir_y
+            if (adj_x < 0 or adj_x >= self.num_rows 
+                or adj_y < 0 or adj_y >= self.num_cols):
+                continue
+            if (self.cells[adj_x][adj_y].visited is False and
+                ((i == 0 and self.cells[row][col].has_right_wall is False) or
+                (i == 1 and self.cells[row][col].has_left_wall is False) or
+                (i == 2 and self.cells[row][col].has_bottom_wall is False) or
+                (i == 3 and self.cells[row][col].has_top_wall is False))):
+                self.cells[row][col].draw_move(self.cells[adj_x][adj_y])
+                if self.solve_r(adj_x, adj_y):
+                    return True
+                else:
+                    self.cells[row][col].draw_move(self.cells[adj_x][adj_y], True)
+        return False
+
+    def solve(self):
+        Cell(self.x1 - self.cell_size_x, self.x1, self.y1, self.y1 + self.cell_size_y, self.win).draw_move(self.cells[0][0])
+        return self.solve_r(0, 0)
+
+
 
 
 
